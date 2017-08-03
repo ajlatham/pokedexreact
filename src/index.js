890192a01c85603d9ba4a61bf3e3210eb7bdb7bc
Will Austin <willaustinrm@gmail.com>
2017-08-02 22:10:58 -0600

    first commmit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+function tick() {
+
+	const element = ( 
+		<div>
+			<h1>Gotta catch em all!</h1>;
+			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+
+		</div>
+
+	);
+	ReactDOM.render(
+		element,
+		document.getElementById('root')
+	);
+
+}
+
+setInterval(tick, 1000);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+registerServiceWorker();
